7c94e89f8bda0f8b6afa22eb097190f1999b470d
Dylan McNamee <dylan.mcnamee@gmail.com>
2021-08-28 11:14:54 -0700

    turtles!

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,33 @@ pub struct Cells {
     pub all_cells: Vec<FluidCell>,
 }
 
+#[derive(Component)]
+pub struct Boat {
+    pub loc: Point2,
+    pub vel: Vec2,
+    pub health: f32, // or some other per-boat state
+    t: Turtle,
+}
+
+pub fn new_boat(x: f32, y: f32, vx: f32, vy: f32) -> Boat {
+    Boat { loc: Point2 {x: x, y: y}, vel: Vec2::new(vx, vy), health: 1., t: new_turtle()}
+}
+
+impl Boat {
+    pub fn render(&mut self) {
+        self.t.direction = (self.vel.y/self.vel.x).atan();
+        self.t.pen_down();
+        self.t.move_to(self.loc.x, self.loc.y);
+        self.t.forward(10.);
+        self.t.turn_right(90.);
+        self.t.forward(10.);
+        self.t.turn_right(90.);
+        self.t.forward(10.);
+        self.t.turn_right(90.);
+        self.t.forward(10.);
+    }
+}
+
 pub struct FluidCell {
     pub flow_v: Vec2,
     pub flow_updates: Vec2, 
@@ -60,8 +87,6 @@ pub struct Particle {
     pub position: Point2,
     pub size: f32,
 }
-// pub struct Turtle {
-// }
 
 impl Particle {
     fn update_pos(&mut self) -> () {
@@ -182,6 +207,53 @@ fn new_cells() -> Cells {
                         });
     }
     Cells{all_cells: ret}
+
+}
+
+pub struct Turtle {
+    loc: Point2,
+    direction: f32,
+    pen_down: bool,
+    color: Color,
+    line_width: f32,
+}
+
+// creates a new turtle at x,y, pen is up
+pub fn new_turtle() -> Turtle {
+    Turtle { loc: Point2 {x: 0., y: 0.}, direction: 0., pen_down: false, line_width: 10., color: WHITE}
+}
+
+impl Turtle {
+    pub fn forward(&mut self, amount: f32) {
+        let (old_x, old_y) = (self.loc.x, self.loc.y);
+        self.loc.x = old_x + amount * self.direction.cos();
+        self.loc.y = old_y + amount * self.direction.sin();
+        if self.pen_down { 
+            draw_line(old_x, old_y, self.loc.x, self.loc.y, self.line_width, self.color);
+        }
+    }
+    pub fn turn_right(&mut self, degrees: f32) {
+        self.direction += degrees;
+    }
+    pub fn turn_left(&mut self, degrees: f32) {
+        self.direction -= degrees;
+    }
+    pub fn pen_down(&mut self) {
+        self.pen_down = true;
+    }
+    pub fn pen_up(&mut self) {
+        self.pen_down = false;
+    }
+    pub fn set_color(&mut self, new_color: Color){
+        self.color = new_color;
+    }
+    pub fn set_line_width(&mut self, new_width: f32) {
+        self.line_width = new_width;
+    }
+    pub fn move_to(&mut self, x: f32, y: f32) {
+        self.loc.x = x;
+        self.loc.y = y;
+    }
 }
 
 fn window_conf() -> Conf {
@@ -201,8 +273,9 @@ fn init_world(world: &mut World) {
 
     world.bulk_add_entity((0..8).map(|_| (new_particle(), )));
     world.add_unique(new_cells()).unwrap();
-    world.add_unique(ParticleDragger{point_x:0.,point_y:0.});
-    world.add_unique(GameModeInfo{game_mode: GameMode::Default});
+    world.add_unique(ParticleDragger{point_x:0.,point_y:0.}).unwrap();
+    world.add_unique(GameModeInfo{game_mode: GameMode::Default}).unwrap();
+    world.add_unique(new_boat(WIDTH as f32 / 2., HEIGHT as f32 / 2., 0.5, 0.)).unwrap();
 }
 
 // Entry point of the program
@@ -278,21 +351,18 @@ fn move_particle(mut particles: ViewMut<Particle>) -> Result<(), GameOver> {
     }
     Ok(())
 }
-fn drag_particles(mut dragger:UniqueViewMut<ParticleDragger>,
+fn drag_particles(mut dragger: UniqueViewMut<ParticleDragger>,
                   mut particles: ViewMut<Particle>,
-                  mut entities: EntitiesViewMut){
+                  mut entities: EntitiesViewMut,
+                  mut player:UniqueViewMut<Boat>,){
     let (mouse_x, mouse_y) = mouse_position();
-    let dragger_radius: f32 = 30.;
     if is_mouse_button_down(MouseButton::Left) {
         dragger.point_x = lerp(dragger.point_x, mouse_x, 0.3);
         dragger.point_y = lerp(dragger.point_y, mouse_y, 0.3);
-        //draw_circle_lines(dragger.point_x, dragger.point_y, dragger_radius, 0.5, GRAY);
-        //draw_line(x1, y1, x2, y2, thickness, color)
-        // for particle in (&mut particles).iter() {
-        //     if pythag_dist(particle.position.x, particle.position.y, dragger.point_x, dragger.point_y) < dragger_radius{
-        //         particle.update_velocity_from_mouse(mouse_x - dragger.point_x, mouse_y - dragger.point_y);
-        //     }
-        // }
+        player.loc.x = dragger.point_x;
+        player.loc.y = dragger.point_y;
+        player.vel.x = 0.2 * (mouse_x - dragger.point_x); // set the velocity vector
+        player.vel.y = 0.2 * (mouse_y - dragger.point_y);
         entities.add_entity((particles,), (new_particle_at(mouse_x, mouse_y, 
                                                             0.2 * (dragger.point_x - mouse_x), 
                                                             0.2 * (dragger.point_y - mouse_y),),));
@@ -300,14 +370,18 @@ fn drag_particles(mut dragger:UniqueViewMut<ParticleDragger>,
         dragger.point_x = mouse_x;
         dragger.point_y = mouse_y;
     }
+    player.render();
     if is_mouse_button_pressed(MouseButton::Right) {
     }
- }
+}
+
 pub fn pythag_dist(x1: f32, y1: f32, x2: f32, y2: f32,) -> f32 {
     let xd = x2 - x1;
     let yd = y2 - y1;
     (xd * xd + yd * yd).sqrt()
 }
+
+// handle key presses for game mode changes
 fn handle_key_presses(mut game_mode: UniqueViewMut<GameModeInfo>) {
     if is_key_pressed(KeyCode::Space){
         if game_mode.game_mode == GameMode::Debug{
@@ -321,6 +395,7 @@ fn handle_key_presses(mut game_mode: UniqueViewMut<GameModeInfo>) {
     }
 }
 
+// debugging utility: draw the grid lines
 fn draw_world_grid(game_mode: UniqueView<GameModeInfo>) {
     if game_mode.game_mode == GameMode::Debug{
         let cell_width: f32 = WIDTH as f32 / CELLS_X as f32;
@@ -357,7 +432,7 @@ fn apply_grid_updates(mut map:UniqueViewMut<Cells>) -> Result<(), GameOver> {
 // render a frame of the world
 // documentation here: https://docs.rs/macroquad/0.3.8/macroquad/
 fn render(particles: View<Particle>, 
-          map:UniqueView<Cells>, 
+          map: UniqueView<Cells>, 
           game_mode: UniqueView<GameModeInfo> ) -> Result<(), GameOver>
 {
     for particle in particles.iter() {
@@ -365,19 +440,20 @@ fn render(particles: View<Particle>,
     }
     let mut cell_x = 0;
     let mut cell_y = 0;
-    for cell in map.all_cells.iter() {
-        if game_mode.game_mode == GameMode:: Debug{
+    if game_mode.game_mode == GameMode:: Debug{
+        for cell in map.all_cells.iter() {
             cell.render(cell_x, cell_y);
-        }
-        cell_x += 1;
-        if cell_x >= CELLS_X {
-            cell_x = 0;
-            cell_y += 1;
+            cell_x += 1;
+            if cell_x >= CELLS_X {
+                cell_x = 0;
+                cell_y += 1;
+            }
         }
     }
     Ok(())
 }
 
+// update each particle's vector according to the flow of the cell it's in
 fn update_particles_vectors(mut particles: ViewMut<Particle>, map:UniqueView<Cells> ) -> Result<(), GameOver> {
     for particle in (&mut particles).iter() {
         let cell_index = particle.get_cell_index();
